34593de6205528a3ccd6018cdcd3edf1950675ee
danielfreitassc <danielfreitassc@gmail.com>
2024-06-06 10:43:30 -0300

    Feat: CRUD completo de fornecedor

---
+++ sistema/backend/src/main/java/com/unisatc/backend/controllers/FornecedorController.java
@@ -0,0 +1,51 @@
+package com.unisatc.backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.unisatc.backend.dtos.FornecedorDTO;
+import com.unisatc.backend.services.FornecedorService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("fornecedor")
+public class FornecedorController {
+    @Autowired
+    private FornecedorService fornecedorService;
+
+
+    @PostMapping
+    public FornecedorDTO createFornecedor(@RequestBody @Valid FornecedorDTO fornecedorDTO) {
+        return fornecedorService.createFornecedor(fornecedorDTO);
+    }
+
+    @GetMapping
+    public List<FornecedorDTO> getAllFornecedores() {
+        return fornecedorService.getAllFornecedores();
+    }
+
+    @GetMapping("{id}")
+    public FornecedorDTO getFornecedorByid(@PathVariable Integer id) {
+        return fornecedorService.getFornecedorById(id);
+    }
+
+    @PutMapping("{id}")
+    public FornecedorDTO updateFornecedor(@PathVariable Integer id, @RequestBody @Valid FornecedorDTO fornecedorDTO) {
+        return fornecedorService.updateFornecedor(id, fornecedorDTO);
+    }
+    
+    @DeleteMapping("{id}")
+    public FornecedorDTO deleteFornecedor(@PathVariable Integer id) {
+        return fornecedorService.deleteFornecedor(id);
+    }
+}
---
+++ sistema/backend/src/main/java/com/unisatc/backend/services/FornecedorService.java
@@ -0,0 +1,57 @@
+package com.unisatc.backend.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.unisatc.backend.dtos.FornecedorDTO;
+import com.unisatc.backend.mappers.FornecedorMapper;
+import com.unisatc.backend.models.FornecedorEntity;
+import com.unisatc.backend.repositories.FornecedorRepository;
+
+@Service
+public class FornecedorService {
+    @Autowired
+    private FornecedorRepository fornecedorRepository;
+    @Autowired
+    private FornecedorMapper fornecedorMapper;
+
+
+    public FornecedorDTO createFornecedor(FornecedorDTO fornecedorDTO) {
+        FornecedorEntity fornecedorEntity = fornecedorMapper.toEntity(fornecedorDTO);
+        FornecedorEntity fornecedor = fornecedorRepository.save(fornecedorEntity);
+        return fornecedorMapper.toDto(fornecedor);
+    }
+
+    public List<FornecedorDTO> getAllFornecedores() {
+        List<FornecedorEntity> fornecedores = fornecedorRepository.findAll();
+        if(fornecedores.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Nenhum fornecedor cadastrado no momento");
+        return fornecedores.stream().map(fornecedorMapper::toDto).toList();
+    }
+    
+    public FornecedorDTO getFornecedorById(Integer id) {
+        Optional<FornecedorEntity> fornecedor = fornecedorRepository.findById(id);
+        if(fornecedor.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Nenhum fornecedor com este ID cadastrado no momento");
+        return fornecedorMapper.toDto(fornecedor.get());
+    }
+
+    public FornecedorDTO updateFornecedor(Integer id, FornecedorDTO fornecedorDTO) {
+        Optional<FornecedorEntity> fornecedor = fornecedorRepository.findById(id);
+        if(fornecedor.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Nenhum fornecedor com este ID cadastrado no momento");
+        FornecedorEntity fornecedorEntity = fornecedorMapper.toEntity(fornecedorDTO);
+        fornecedorEntity.setId(id);
+        FornecedorEntity updateFornecedor = fornecedorRepository.save(fornecedorEntity);
+        return fornecedorMapper.toDto(updateFornecedor);
+    }
+
+    public FornecedorDTO deleteFornecedor(Integer id) {
+        Optional<FornecedorEntity> fornecedor = fornecedorRepository.findById(id);
+        if(fornecedor.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Nenhum fornecedor com este ID cadastrado no momento");
+        fornecedorRepository.delete(fornecedor.get());
+        return fornecedorMapper.toDto(fornecedor.get());
+    }
+}
