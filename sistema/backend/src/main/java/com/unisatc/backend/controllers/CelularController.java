eb690811daa05301afbe4bf5f111bab091ad2069
danielfreitassc <danielfreitassc@gmail.com>
2024-06-06 01:44:14 -0300

    Feat: CRUD completo da entidade celular

---
+++ sistema/backend/src/main/java/com/unisatc/backend/controllers/CelularController.java
@@ -0,0 +1,50 @@
+package com.unisatc.backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.unisatc.backend.dtos.CelularDTO;
+import com.unisatc.backend.services.CelularService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("celular")
+public class CelularController {
+    @Autowired
+    private CelularService celularService;
+
+    @PostMapping
+    public CelularDTO createCelular(@RequestBody @Valid CelularDTO celularDTO) {
+        return celularService.createCelular(celularDTO);
+    }
+
+    @GetMapping
+    public List<CelularDTO> getAllCelulares() {
+        return celularService.getAllCelulares();
+    }
+
+    @GetMapping("{id}")
+    public CelularDTO getCelularById(@PathVariable Integer id) {
+        return celularService.getCelularByID(id);
+    }
+
+    @PutMapping("{id}")
+    public CelularDTO updateCelular(@PathVariable Integer id, @RequestBody @Valid CelularDTO celularDTO) {
+        return celularService.updateCelular(id, celularDTO);
+    }
+
+    @DeleteMapping("{id}")
+    public CelularDTO deleteCelular(@PathVariable Integer id) {
+        return celularService.deleteCelular(id);
+    }
+}
---
+++ sistema/backend/src/main/java/com/unisatc/backend/services/CelularService.java
@@ -0,0 +1,57 @@
+package com.unisatc.backend.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.unisatc.backend.dtos.CelularDTO;
+import com.unisatc.backend.mappers.CelularMapper;
+import com.unisatc.backend.models.CelularEntity;
+import com.unisatc.backend.repositories.CelularRepository;
+
+@Service
+public class CelularService {
+    @Autowired
+    private CelularRepository celularRepository;
+    @Autowired
+    private CelularMapper celularMapper;
+
+
+    public CelularDTO createCelular(CelularDTO celularDTO) {
+        CelularEntity celularEntity = celularMapper.toEntity(celularDTO);
+        CelularEntity celular = celularRepository.save(celularEntity);
+        return celularMapper.toDto(celular);
+    }
+
+    public List<CelularDTO> getAllCelulares() {
+        List<CelularEntity> celulares = celularRepository.findAll();
+        if(celulares.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Nenhum celular cadastrado no momento");
+        return celulares.stream().map(celularMapper::toDto).toList();
+    }
+
+    public CelularDTO getCelularByID(Integer id) {
+        Optional<CelularEntity> celular = celularRepository.findById(id);
+        if(celular.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Nenhum celular com este ID cadastrado no momento");
+        return celularMapper.toDto(celular.get());
+    }
+
+    public CelularDTO updateCelular(Integer id, CelularDTO celularDTO) {
+        Optional<CelularEntity> celular = celularRepository.findById(id);
+        if(celular.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Nenhum celular com este ID cadastrado no momento");
+        CelularEntity celularEntity = celularMapper.toEntity(celularDTO);
+        celularEntity.setId(id);
+        CelularEntity updateCelular = celularRepository.save(celularEntity);
+        return celularMapper.toDto(updateCelular);
+    }
+    
+    public CelularDTO deleteCelular(Integer id) {
+        Optional<CelularEntity> celular = celularRepository.findById(id);
+        if(celular.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Nenhum celular com este ID cadastrado no momento");
+        celularRepository.delete(celular.get());
+        return celularMapper.toDto(celular.get());
+    }
+}
